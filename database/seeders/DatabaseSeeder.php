fc38ecae6cd885540dd1ccecd1b2a8f1e2669bc7
Kelechi Okwuriki <kelechi@bento.africa>
2021-08-03 01:01:38 +0100

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // patients - 1000
+        for ($i = 0; $i < 1000; $i++) {
+            User::create([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'role' => 'patient'
+            ]);
+        }
+
+        // practice staff - admin (30)
+        for ($i = 0; $i < 30; $i++) {
+            User::create([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'role' => 'admin'
+            ]);
+        }
+
+        // practice staff - nurse (35)
+        for ($i = 0; $i < 35; $i++) {
+            User::create([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'role' => 'nurse'
+            ]);
+        }
+
+        // practice staff - doctor (35)
+        for ($i = 0; $i < 35; $i++) {
+            User::create([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'role' => 'doctor'
+            ]);
+        }
     }
 }
